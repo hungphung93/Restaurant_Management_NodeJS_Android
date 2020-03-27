34c9aa27270861567def1aad7e34799daedd0588
hansonphung <n01312493@humbermail.ca>
2020-03-27 00:47:00 -0400

    Filter not to get deleted foods

--- Backend/RestaurantManagementAPI/src/repositories/foodRepositories.js
+++ Backend/RestaurantManagementAPI/src/repositories/foodRepositories.js
@@ -2,12 +2,14 @@ import { FoodEntity } from '../entities/FoodEntity';
 
 export const addNewFood = async (food) => {
     try {
+        console.log(food.foodDescription);
         let newFood = new FoodEntity();
         newFood.name = food.foodName;
         newFood.description = food.foodDescription;
         newFood.image_url = food.imageURL;
         newFood.category = food.category;
         newFood.price = food.price;
+
 
         const createdFood = await newFood.save();
 
@@ -19,7 +21,7 @@ export const addNewFood = async (food) => {
 
 export const getAllFoods = async () => {
     try {
-        let lstFood = await FoodEntity.find();
+        let lstFood = await FoodEntity.find({ is_deleted: false });
 
         return await lstFood;
     } catch (err) {
