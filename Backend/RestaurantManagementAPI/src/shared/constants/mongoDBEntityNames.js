691c4562a06321355f1dc431a72bc9b485c7e352
hansonphung <n01312493@humbermail.ca>
2020-03-27 00:45:22 -0400

    Add Food entity to create database collection and implement food repositories to add food and get all foods

--- Backend/RestaurantManagementAPI/src/shared/constants/mongoDBEntityNames.js
+++ Backend/RestaurantManagementAPI/src/shared/constants/mongoDBEntityNames.js
@@ -1,3 +1,5 @@
 export const userCollection = "users";
 export const tableCollection = "tables";
 export const foodCollection = "foods";
+export const transactionCollection = "transactions";
+export const orderedFoodCollection = "ordered_foods";
