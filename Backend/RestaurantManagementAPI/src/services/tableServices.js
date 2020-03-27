1cb4df280df04c9db1d3aabd070df2ee01e00463
hansonphung <n01312493@humbermail.ca>
2020-03-27 00:49:54 -0400

    Implement service to open new table or get table details

--- Backend/RestaurantManagementAPI/src/services/tableServices.js
+++ Backend/RestaurantManagementAPI/src/services/tableServices.js
@@ -1,5 +1,6 @@
 import * as tableRepositories from '../repositories/tableRepositories';
 import { Table } from '../models/Table';
+import { Transaction } from '../models/Transaction';
 
 export const getAllTables = async () => {
     try {
@@ -20,17 +21,31 @@ export const createTable = async (tableName) => {
     try {
         let createdTable = await tableRepositories.createTable(tableName);
 
-        return new Table(createdTable._id, createdTable.name, createdTable.status, createdTable.open_at);
+        return await new Table(createdTable._id, createdTable.name, createdTable.status, createdTable.open_at);
     } catch (err) {
         throw err;
     }
 }
 
-export const getTableDetail = async (tableId) => {
+export const getTableDetail = async (tableName) => {
     try {
-        let tableInfo = await tableRepositories.getTableDetail(tableId);
+        let tableInfo = await tableRepositories.getTableDetail(tableName);
 
-        return new Table(tableInfo._id, tableInfo.name, tableInfo.status, tableInfo.open_at);
+        return await new Transaction(tableInfo._id, tableInfo.table_name,
+            tableInfo.status,
+            tableInfo.open_at,
+            tableInfo.orderedFoods,
+            tableInfo.total_amount);
+    } catch (err) {
+        throw err;
+    }
+}
+
+export const openTable = async (tableName) => {
+    try {
+        let isOpened = await tableRepositories.openTable(tableName);
+
+        return await isOpened;
     } catch (err) {
         throw err;
     }
