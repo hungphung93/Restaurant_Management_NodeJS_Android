d9955e0a8e57c5309b6e220e7d1c039945058aba
hansonphung <n01312493@humbermail.ca>
2020-03-31 11:56:44 -0400

    Add menu panel

--- Frontend/app/src/main/java/com/example/restaurantmanagement/Activities/TableListActivity.java
+++ Frontend/app/src/main/java/com/example/restaurantmanagement/Activities/TableListActivity.java
@@ -1,9 +1,15 @@
 package com.example.restaurantmanagement.Activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.restaurantmanagement.Enums.TableStatus;
@@ -15,13 +21,16 @@ import com.example.restaurantmanagement.Models.OpenTableRequest;
 import com.example.restaurantmanagement.Models.Table;
 import com.example.restaurantmanagement.R;
 import com.example.restaurantmanagement.Services.Implementation.TableServices;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
-public class TableListActivity extends AppCompatActivity implements ITableListEventListener {
+public class TableListActivity extends AppCompatActivity implements ITableListEventListener, NavigationView.OnNavigationItemSelectedListener {
 
     private String selectedTable = "";
     private final String TABLE_NAME = "TableName";
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +38,18 @@ public class TableListActivity extends AppCompatActivity implements ITableListEv
 
             super.onCreate(savedInstanceState);
             setContentView(R.layout.table_list);
+
+            Toolbar toolbar = findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+
+            drawer = findViewById(R.id.draw_layout);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,  toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+            NavigationView navigationView = findViewById(R.id.nav_view);
+            navigationView.setNavigationItemSelectedListener(this);
+
+            drawer.addDrawerListener(toggle);
+            toggle.syncState();
 
             ApiResponse<ArrayList<Table>> lstTables = TableServices.getAllTables();
             lstTables.Subscribe(this::handleGetTablesSuccess, this::handleAPIFailure);
@@ -39,6 +60,14 @@ public class TableListActivity extends AppCompatActivity implements ITableListEv
     }
 
     @Override
+    public void onBackPressed(){
+        if(drawer.isDrawerOpen(GravityCompat.START))
+            drawer.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
+    }
+
+    @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
 
     }
@@ -46,9 +75,6 @@ public class TableListActivity extends AppCompatActivity implements ITableListEv
     @Override
     public void onTableStatusClick(Table table) {
         try{
-
-            //Toast.makeText(this, String.format("This table is %s", table.getTableStatus()) , Toast.LENGTH_SHORT).show();
-
             selectedTable = table.getTableName();
 
             if(table.getTableStatus().equals(TableStatus.EMPTY.toString()))
@@ -113,5 +139,23 @@ public class TableListActivity extends AppCompatActivity implements ITableListEv
     private void handleAPIFailure(Throwable t){
         Toast.makeText(this, "Internal error happened. Please try later.",
                 Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Intent intent = new Intent();
+        switch(menuItem.getItemId()){
+            case R.id.nav_menu:
+                intent = new Intent(this, TableListActivity.class);
+                break;
+            case R.id.nav_table:
+                intent = new Intent(this, TableListActivity.class);
+                break;
+            case R.id.nav_food:
+                intent = new Intent(this, OrderListActivity.class);
+        }
+
+        startActivity(intent);
+        return true;
     }
 }
